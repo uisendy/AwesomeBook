d771d88e61ae868d5e7a318473d2d4e7fecbf5dd
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-08 15:52:26 +0100

    ES6 Syntax LocalStore.IdGenerator Method,

--- module/LocalStorage.js
+++ module/LocalStorage.js
@@ -1,9 +1,9 @@
 class LocalStore {
-  static idGenerator() {
+  static idGenerator = () => {
     const books = LocalStore.getBooks();
     const id = books.length ? books[books.length - 1].id + 1 : 1;
     return id;
-  }
+  };
 
   static getBooks = () => (localStorage.getItem('BookDetails')
     ? JSON.parse(localStorage.getItem('BookDetails'))
